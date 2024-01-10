5381ee55078eae701221d20ab580976daa337376
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2024-01-10 15:48:57 +0300

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -34,12 +34,12 @@ def matrix_mul(m_a, m_b):
     for row in m_a:
         row_len.append(len(row))
     if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a should be of the same size")
     row_len = []
     for row in m_b:
         row_len.append(len(row))
     if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b should be of the same size")
 
     a_col = 0
     for col in m_a[0]:
