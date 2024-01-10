edd93c38c5a1ad738100e7261e18ecd83e86293d
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2024-01-10 15:35:06 +0300

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-""" This module provides a function that prints a square. """
+# 4-print_square.py
 
 def print_square(size):
     """
@@ -23,3 +22,28 @@ def print_square(size):
     for _ in range(size):
         print("#" * size)
 
+# 4-main.py
+
+#!/usr/bin/python3
+print_square = __import__('4-print_square').print_square
+
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+
+# tests/4-print_square.txt
+
+# No doctests in this case
+
+# Run the tests
+# guillaume@ubuntu:~/0x07$ python3 -m doctest -v ./tests/4-print_square.txt
