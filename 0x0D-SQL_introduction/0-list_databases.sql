8e93bc8d14aa742101c21306aad279444b51a6bf
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2024-02-14 08:47:53 +0300

    Update 0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,9 +1,3 @@
--- 0-list_databases.sql
-
--- Connect to MySQL server
--- Replace 'your_username' and 'your_password' with your MySQL username and password
--- You may also need to replace 'localhost' with your MySQL server address if it's different
-mysql -u your_username -p your_password -h localhost <<EOF
 
 -- Lists all databases
 SHOW DATABASES;
